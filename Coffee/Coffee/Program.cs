6a627382be96bc5102bb4582cbdc3e814c9fc0e0
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-19 09:06:22 +0000

    Files added for homework

---
+++ Coffee/Coffee/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class BasicCoffeeMachine : IBrewCoffee, IBlend, IDecaf, IMilk
+    {
+
+    }
+    class PremiumCoffeeMachine
+    {
+
+    }
+    interface IBeans
+    {
+        //Light Roast, Medium Roast, Dark Roast
+    }
+    interface IDecaf
+    {
+        //Full Strength
+    }
+    interface IMilk
+    {
+        //Milky or Black
+    }
+    interface IBlend
+    {
+        //Instant of Brewed
+    }
+    interface IBrewCoffee
+    {
+        //Make Coffee
+    }
+}
